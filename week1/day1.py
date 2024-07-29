d10927c815f53559a3e47cff274878fffe41cef3
Harsh <harsh.valecha@yahoo.com>
2024-07-30 00:25:36 +0530

    added day 1 file with 2 tasks

---
+++ week1/day1.py
@@ -0,0 +1,13 @@
+# task 1 - create a python program to take input of a value and print the table for it
+
+
+def print_table(value:int):
+    for i in range(1,11):
+        print(f'{value} X {i} = {value*i}')
+
+
+# value = int(input('Enter value:'))
+# print_table(value)
+
+# task 2 - print all the functions available for string datatype
+# print(dir(str))
