dee72592187fd9b9239b6020d1f6e9a06780107a
Harsh <harsh.valecha@yahoo.com>
2024-08-10 18:53:56 +0530

    type,keyword,complex numbers

---
+++ src/ex_10082024/Lab004.py
@@ -0,0 +1,6 @@
+# program to find maximum number between 2 numbers
+print(max(34,100,102)) # max returns the maximum number
+print(max(1,-1,0,100,20,23.34))
+
+#print(max(1,-1,0,100,20,23.34,"harsh")) -- str and int cannot be compared
+
---
+++ src/ex_10082024/Lab005.py
@@ -0,0 +1,11 @@
+import keyword
+print(keyword.kwlist)
+#['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+
+# checking type of variables
+print(type(12),type('hello'),type(True))
+complex_number = 2+3j
+
+print(complex_number.real)
+print(complex_number.imag)
