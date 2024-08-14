07cd78873e9c97ca2c92ee842455b2682711e53d
Harsh <harsh.valecha@yahoo.com>
2024-08-15 00:40:33 +0530

    Homework 13.08.2024

--- src/ex_13082024/Lab010.py
+++ src/ex_13082024/Lab010.py
@@ -1,17 +1,17 @@
 # create a Program I will give you number(userinput and print table)
 
-# num = int(input('Please enter the number :'))
-#
-# print(f'{num} * 1 = {num*1}')
-# print(f'{num} * 2 = {num*2}')
-# print(f'{num} * 3 = {num*3}')
-# print(f'{num} * 4 = {num*3}')
-# print(f'{num} * 5 = {num*4}')
-# print(f'{num} * 6 = {num*5}')
-# print(f'{num} * 7 = {num*6}')
-# print(f'{num} * 8 = {num*7}')
-# print(f'{num} * 9 = {num*9}')
-# print(f'{num} * 10 = {num*10}')
+num = int(input('Please enter the number :'))
+
+print(f'{num} * 1 = {num*1}')
+print(f'{num} * 2 = {num*2}')
+print(f'{num} * 3 = {num*3}')
+print(f'{num} * 4 = {num*3}')
+print(f'{num} * 5 = {num*4}')
+print(f'{num} * 6 = {num*5}')
+print(f'{num} * 7 = {num*6}')
+print(f'{num} * 8 = {num*7}')
+print(f'{num} * 9 = {num*9}')
+print(f'{num} * 10 = {num*10}')
 
 
 #Create a program , take 2 inputs from the user num1, num2 and give them - max , pow , sub , mul
